10033f94570f4b03a770b7086cfa55bb83b46b5b
Jonathan <vanillajonathan@users.noreply.github.com>
2024-02-05 10:54:08 +0100

    Update User.cs
    Use modern file-scoped namespaces.

--- assets/snippets/User.cs
+++ assets/snippets/User.cs
@@ -1,20 +1,19 @@
 using System;
 using System.Text.Json.Serialization;
 
-namespace Contoso.Example
+namespace Contoso.Example;
+
+public class User
 {
-    public class User
-    {
-        [JsonPropertyName("createdAt")]
-        public DateTimeOffset CreatedAt { get; set; }
+    [JsonPropertyName("createdAt")]
+    public DateTimeOffset CreatedAt { get; set; }
 
-        [JsonPropertyName("id")]
-        public string Id { get; set; }
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
 
-        [JsonPropertyName("isAdmin")]
-        public bool IsAdmin { get; set; }
+    [JsonPropertyName("isAdmin")]
+    public bool IsAdmin { get; set; }
 
-        [JsonPropertyName("karma")]
-        public int Karma { get; set; }
-    }
+    [JsonPropertyName("karma")]
+    public int Karma { get; set; }
 }
